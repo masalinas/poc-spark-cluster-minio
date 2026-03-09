59c15becdddcb46e88af01a5266956648619b12a
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2026-03-09 10:11:19 +0100

    last commit

--- src/transform-hdf-to-parquet.py
+++ src/transform-hdf-to-parquet.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-H5_FILE = "/Users/miguel/git/poc-spark-cluster-minio/datasets/df_data.hdf"
-PARQUET_FILE= "/Users/miguel/git/poc-spark-cluster-minio/datasets/df_data.parquet"
+#H5_FILE = "/Users/miguel/git/poc-spark-cluster-minio/datasets/df_data.hdf"
+#PARQUET_FILE= "/Users/miguel/git/poc-spark-cluster-minio/datasets/df_data.parquet"
+
+H5_FILE = "/home/miguel/git/poc-spark-cluster-minio/datasets/df_data.hdf"
+PARQUET_FILE= "/home/miguel/git/poc-spark-cluster-minio/datasets/df_data.parquet"
 
 print("Reading HDF5...")
 df = pd.read_hdf(H5_FILE)
